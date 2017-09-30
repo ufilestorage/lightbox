b185d87a54086efe9e51d95e0aec4a15da076f4c
Uplayer Stream <31379531+UPlayerStream@users.noreply.github.com>
2017-09-30 22:58:05 +0700

    Add files via upload

---
+++ config/autoload.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * @copyright  inspiredminds.at 2015
+ * @author     Fritz Michael Gschwantner <fmg@inspiredminds.at>
+ * @package    nivo_lightbox
+ * @license    GNU GPL v2.0
+ */
+
+
+/**
+ * Register the templates
+ */
+TemplateLoader::addFiles(array
+(
+	'j_nivo_lightbox' => 'system/modules/nivo_lightbox/templates',
+));
